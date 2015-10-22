145d7e38d55ebad04968051e99429eb29d7f5138
Zach Toogood <zachary.toogood@concentra.co.uk>
2015-10-22 15:08:08 +0100

    Some more strange colors

--- include/graphics/Color.h
+++ include/graphics/Color.h
@@ -1,3 +1,4 @@
+//https://github.com/libgdx/libgdx/blob/master/gdx/src/com/badlogic/gdx/graphics/Color.java
 #pragma once
 
 #include "common.h"
@@ -11,13 +12,20 @@
 
 #define BLUE vec3(0.0f, 0.0f, 1.0f)
 #define NAVY vec3(0.0f, 0.0f, 0.5f)
+#define ROYAL vec3(0.255f, 0.412f, 0.882f)
+#define SKY vec3(0.529f, 0.808f, 0.922f)
 #define CYAN vec3(0.0f, 1.0f, 1.0f)
 #define TEAL vec3(0.0f, 0.5f, 0.5f)
 
 #define GREEN vec3(0.0f, 1.0f, 0.0f)
+#define CHARTREUSE vec3(0.498f, 1.000f, 0.000f) 
 #define LIME vec3(0.196f, 0.804f, 0.196f)
+#define FOREST vec3(0.133f, 0.545f, 0.133f)
+#define OLIVE vec3(0.502f, 0.502f, 0.000f)
 
 #define YELLOW vec3(1.0f, 1.0f, 0.0f)
+#define GOLD vec3(1.000f, 0.843f, 0.000f)
+#define GOLDENROD vec3(0.855f, 0.647f, 0.12f)
 
 #define BROWN vec3(0.545f, 0.271f, 0.075f)
 #define TAN vec3(0.824f, 0.706f, 0.549f)
@@ -26,9 +34,28 @@
 #define RED vec3(1.0f, 0.0f, 0.0f)
 #define CORAL vec3(1.0f, 0.498f, 0.314f)
 #define ORANGE vec3(1.0f, 0.647f, 0.0f)
+#define SALMON vec3(0.980f, 0.502f, 0.447f)
 #define PINK vec3(1.0f, 0.412f, 0.706f)
-
 #define MAGENTA vec3(1.0f, 0.0f, 1.0f)
 
 #define PURPLE vec3(0.627f, 0.125f, 0.941f)
+#define VIOLET vec3(0.933f, 0.510f, 0.933f)
+#define MAROON vec3(0.502f, 0.000f, 0.000f)
 
+// http://prideout.net/archive/colors.php
+
+// Strange color names : http://mentalfloss.com/article/53121/11-colors-youve-probably-never-heard
+// Sarcoline (Skin Color)
+// Coquelicot (Orange)
+// Smaragdine (Blue/Green)
+// Mikado (Yellow/Gold)
+// Glaucous (Powder Blue)
+// Wenge (Grey/Brown)
+// Fulvous (Brown/Yellow)
+// Xanadu (Grey/Green)
+// Falu (Copper/Red)
+// Amaranth (Pink/Red)
+
+#define RAZZAMATAZZ vec3(0.89f, 0.145f, 0.42f)
+
+#define ELECTRICLIME vec3(0.8f, 1.0f, 0.0f)
