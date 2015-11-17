3f6a92058910e50e9ed091b48b202ca8faf231b9
Zach Toogood <zachary.toogood@concentra.co.uk>
2015-11-17 17:39:21 +0000

    Finished updating modules

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,15 @@
 #include "graphics/GL.h"
 #include "graphics/glm.h"
 
-#include "graphics/Window2D.h"
+#include "graphics/Window.h"
 
-auto window = Window2D("OpenGL 2D Skeleton", 1280, 720);
+auto window = Window("OpenGL 2D Skeleton", 1280, 720);
 
 void step () {
 
 	window.update();
 
 	window.clear();
-
-	for (int i = 0; i < 200; ++i)
-	{
-		window.drawPixel(10 + i, 10 + i, 255, 255, 255);
-	}	
 
 	window.swap();
 }
